5421592ab1e55ee1183cbd0b5376d15b294b3275
tsukigahara <satsuki.tksm@gmail.com>
2022-12-11 21:27:37 +0100

    added new components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppTitle />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
@@ -20,9 +14,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div>
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
